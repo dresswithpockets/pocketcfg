f0f4f405ab2acc2a893fe706a56e66d0ee975c50
Ashley <dresswithpockets@pm.me>
2021-05-03 23:39:39 -0700

    inital commit

---
+++ PocketCfg/PocketCfg.Pkt/Lexer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PocketCfg.Pkt
+{
+    public class Lexer
+    {
+    }
+
+    public class Token
+    {
+        
+    }
+
+    public enum TokenType
+    {
+        Identifier,
+        Value,
+        Quote,
+        Comment
+    }
+}
